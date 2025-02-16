d1de7356bce3ad1efbd001a34f04beb842385902
lodjers <officialkovtun@gmail.com>
2025-02-16 18:45:34 +0300

    Убрал AuthProviderImpl, теперь Spring Security сам сравнивает полученное имя пользователя с именем пользователя из БД, сравнивает пароль и возвращает Authentication с Principal-ом внутри

--- src/main/java/ru/lodjers/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/lodjers/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -1,24 +1,30 @@
 package ru.lodjers.springcourse.FirstSecurityApp.config;
 
-import ru.lodjers.springcourse.FirstSecurityApp.security.AuthProviderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.lodjers.springcourse.FirstSecurityApp.services.PersonDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private final AuthProviderImpl authProvider;
 
+    private final PersonDetailsService personDetailsService;
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
 
-        auth.authenticationProvider(authProvider);
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
